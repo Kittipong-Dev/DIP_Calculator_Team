63f7a4e5c54ed5693cf1b7c511ce2fe84e92ddfa
kittipong-dev <achikrab@gmail.com>
2024-09-07 16:46:54 +0700

    declear all Calculator method

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+# don't forget cannot divide by zero
 class Calculator:
     def __init__(self) -> None:
         self.left_operand: str = "0"
@@ -5,3 +6,33 @@ class Calculator:
         self.operator: str | None = None
         self.left_operand_is_decimal: bool = False
         self.right_operand_is_decimal: bool = False
+
+    def plus(self) -> str:
+        pass
+    
+    def minus(self) -> str:
+        pass
+    
+    def divide(self) -> str:
+        pass
+    
+    def multiply(self) -> str:
+        pass
+
+    def percent(self) -> str:
+        pass
+
+    def negate(self) -> str:
+        pass
+
+    def decimal(self) -> str:
+        pass
+
+    def get_result(self) -> str:
+        pass
+    
+    def add_number(self, number: str) -> str:
+        pass
+
+    def clear(self) -> str:
+        pass
